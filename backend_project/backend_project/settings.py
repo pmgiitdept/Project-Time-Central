2a91e912160895c7fd2073a9f50d4412874d097e
pmgiitdept <pmgi.itdept@gmail.com>
2025-09-11 14:59:22 +0800

    Update settings.py

--- backend_project/backend_project/settings.py
+++ backend_project/backend_project/settings.py
@@ -55,8 +55,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",   # right after security
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
