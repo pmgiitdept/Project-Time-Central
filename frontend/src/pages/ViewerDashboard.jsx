deea59e6221678df689cf97a53bdf8c0e3d3446a
pmgiitdept <pmgi.itdept@gmail.com>
2025-09-11 11:11:21 +0800

    Update ViewerDashboard.jsx

--- frontend/src/pages/ViewerDashboard.jsx
+++ frontend/src/pages/ViewerDashboard.jsx
@@ -17,11 +17,11 @@ export default function ViewerDashboard() {
       {/* Navigation Bar */}
       <nav className="navbar">
         <div className="navbar-left">
-          <img src="/src/pmgi.png" alt="Logo" className="navbar-logo" />
+          <img src="/pmgi.png" alt="Logo" className="navbar-logo" />
           <h1 className="navbar-title">Viewer Dashboard</h1>
         </div>
         <div className="navbar-right">
-          <img src="/src/sgslogos.png" alt="Right Logo" className="navbar-logo" />
+          <img src="/sgslogos.png" alt="Right Logo" className="navbar-logo" />
           <LogoutButton />
         </div>
       </nav>
