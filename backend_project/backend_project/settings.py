4dd776555a7e103ef72ec5beb58d0a8b2492a6b2
pmgiitdept <pmgi.itdept@gmail.com>
2025-09-19 13:44:35 +0800

    Update settings.py

--- backend_project/backend_project/settings.py
+++ backend_project/backend_project/settings.py
@@ -93,12 +93,12 @@ CHANNEL_LAYERS = {
     },
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # local dev
     "https://project-time-central-1.onrender.com",  # frontend in Render
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
